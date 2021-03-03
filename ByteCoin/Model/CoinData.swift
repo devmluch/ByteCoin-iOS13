d4493bef339bbbfcab31a63662c7e218fc9095f2
devmluch <mluchina@sparkdigital.com>
2021-03-03 01:20:40 -0300

    First commit

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,15 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Mateo Luchina on 02/03/2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
